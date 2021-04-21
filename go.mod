525a1378923cf3767e899378024d7663b617868a
Tamal Saha <tamal@appscode.com>
2021-04-21 06:23:15 -0700

    added-all

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/Masterminds/sprig/v3 v3.2.2
-	k8s.io/client-go v0.21.0
+	gomodules.xyz/jsonpath v0.0.1
 )
--- main.go
+++ main.go
@@ -8,44 +8,79 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
-	"k8s.io/client-go/util/jsonpath"
+	"gomodules.xyz/jsonpath"
 )
 
 // https://kubernetes.io/docs/reference/kubectl/jsonpath/
 
 var data = `{
-  "kind": "List",
-  "items":[
-    {
-      "kind":"None",
-      "metadata":{"name":"127.0.0.1"},
-      "status":{
-        "capacity":{"cpu":"4"},
-        "addresses":[{"type": "LegacyHostIP", "address":"127.0.0.1"}]
+  "apiVersion": "apps/v1",
+  "kind": "Deployment",
+  "metadata": {
+    "annotations": {
+      "deployment.kubernetes.io/revision": "1"
+    },
+    "labels": {
+      "app": "busy-dep"
+    },
+    "name": "busy-dep",
+    "namespace": "default"
+  },
+  "spec": {
+    "progressDeadlineSeconds": 600,
+    "replicas": 1,
+    "revisionHistoryLimit": 10,
+    "selector": {
+      "matchLabels": {
+        "app": "busy-dep"
       }
     },
-    {
-      "kind":"None",
-      "metadata":{"name":"127.0.0.2"},
-      "status":{
-        "capacity":{"cpu":"8"},
-        "addresses":[
-          {"type": "LegacyHostIP", "address":"127.0.0.2"},
-          {"type": "another", "address":"127.0.0.3"}
-        ]
+    "strategy": {
+      "rollingUpdate": {
+        "maxSurge": "25%",
+        "maxUnavailable": "25%"
+      },
+      "type": "RollingUpdate"
+    },
+    "template": {
+      "metadata": {
+        "creationTimestamp": null,
+        "labels": {
+          "app": "busy-dep"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "command": [
+              "sleep",
+              "3600"
+            ],
+            "image": "busybox",
+            "imagePullPolicy": "IfNotPresent",
+            "name": "busybox",
+            "terminationMessagePath": "/dev/termination-log",
+            "terminationMessagePolicy": "File"
+          },
+          {
+            "command": [
+              "sleep",
+              "3600"
+            ],
+            "image": "ubuntu:18.04",
+            "imagePullPolicy": "IfNotPresent",
+            "name": "ubuntu",
+            "terminationMessagePath": "/dev/termination-log",
+            "terminationMessagePolicy": "File"
+          }
+        ],
+        "dnsPolicy": "ClusterFirst",
+        "restartPolicy": "Always",
+        "schedulerName": "default-scheduler",
+        "terminationGracePeriodSeconds": 30
       }
     }
-  ],
-  "users":[
-    {
-      "name": "myself",
-      "user": {}
-    },
-    {
-      "name": "e2e",
-      "user": {"username": "admin", "password": "secret"}
-    }
-  ]
+  }
 }`
 
 func jpfn(expr string, data interface{}, jsonoutput ...bool) (interface{}, error) {
@@ -72,6 +107,25 @@ func jpfn(expr string, data interface{}, jsonoutput ...bool) (interface{}, error
 	return buf.String(), err
 }
 
+func main2(d interface{}) error {
+	path := jsonpath.New("jp")
+	expr := "{.metadata.labels}"
+	if err := path.Parse(expr); err != nil {
+		return fmt.Errorf("unrecognized column definition %q", expr)
+	}
+	path.AllowMissingKeys(true)
+
+	results, err := path.FindResults(d)
+	if err != nil {
+		return err
+	}
+
+	x := results[0][0].Interface()
+	fmt.Println(x)
+
+	return nil
+}
+
 func main() {
 	var d interface{}
 	err := json.Unmarshal([]byte(data), &d)
@@ -79,12 +133,15 @@ func main() {
 		panic(err)
 	}
 
+	//err = main2(d)
+	//if err != nil {
+	//	panic(err)
+	//}
+
 	fm := sprig.TxtFuncMap()
 	fm["jp"] = jpfn
 
-	tpl := template.Must(template.New("").Funcs(fm).Parse(`
-{{ jp "{.items}" . }}
-`))
+	tpl := template.Must(template.New("").Funcs(fm).Parse(`{{ jp "{.metadata.labels}" . }}`))
 	err = tpl.Execute(os.Stdout, d)
 	if err != nil {
 		panic(err)
