c237444bfb7c615588989637c9c04d5c07918cf3
Tamal Saha <tamal@appscode.com>
2021-04-21 00:50:28 -0700

    added-all

--- main.go
+++ main.go
@@ -1,47 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"os"
 	"text/template"
+
 	"github.com/Masterminds/sprig/v3"
 	"k8s.io/client-go/util/jsonpath"
 )
 
-func main2() {
-	jp := jsonpath.New(col.Name)
-	if err := jp.Parse(col.JSONPath); err != nil {
-		return nil, fmt.Errorf("unrecognized column definition %q", col.JSONPath)
+var data = map[string]interface{}{
+	"a": map[string]interface{}{
+		"b": []interface{}{
+			"c",
+			"d",
+		},
+	},
+}
+
+func jpfn(expr string, data interface{}) (interface{}, error) {
+	jp := jsonpath.New("jp")
+	if err := jp.Parse(expr); err != nil {
+		return nil, fmt.Errorf("unrecognized column definition %q", expr)
 	}
 	jp.AllowMissingKeys(true)
+	// jp.EnableJSONOutput(true)
 
+	var buf bytes.Buffer
+	err := jp.Execute(&buf, data)
+	if err != nil {
+		return nil, err
+	}
+
+	var v interface{}
+	err = json.Unmarshal(buf.Bytes(), &v)
+	return v, err
 }
 
 func main() {
-	type Inner struct {
-		A string
-	}
-	type Outer struct {
-		Inner
-	}
+	fm := sprig.TxtFuncMap()
+	fm["jp"] = jpfn
 
-	type NA struct {
-		O []Outer
-	}
-
-	na := NA{
-		O: []Outer{
-			{
-				Inner: Inner{A: "123"},
-			},
-		},
-	}
-	tpl := template.Must(template.New("").Funcs(sprig.TxtFuncMap()).Parse(`
-{{ define "t2" }}
-{{ printf "%v" .A }}
-{{ end }}
-{{ range $svc := .O }}
-	{{ template "t2" $svc }}
-{{ end }}
+	tpl := template.Must(template.New("").Funcs(fm).Parse(`
+{{ jp "{.a.b}" . | len }}
 `))
-	tpl.Execute(os.Stdout, &na)
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
 }
